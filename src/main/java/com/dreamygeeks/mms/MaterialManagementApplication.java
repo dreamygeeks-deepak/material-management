cca656964bbe7de15678cb94f5b497a6ee1495b6
deepakshingan <deepak.shingan@gmail.com>
2017-09-15 12:13:38 +0530

    added company and dashboard fxml

--- src/main/java/com/dreamygeeks/mms/MaterialManagementApplication.java
+++ src/main/java/com/dreamygeeks/mms/MaterialManagementApplication.java
@@ -1,10 +1,9 @@
-package fxmlexample;
+package com.dreamygeeks.mms;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,33 +11,23 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MaterialManagementApplication extends Application {
-
+    private ConfigurableApplicationContext springContext;
     private Parent root;
+
 	public static void main(String[] args) {
-        System.out.println("I am in main");
         launch(MaterialManagementApplication.class, args);
 	}
 
-	private ConfigurableApplicationContext springContext;
-
 	@Override
 	public void init() throws Exception {
-        System.out.println("I am in init");
-
         springContext = SpringApplication.run(MaterialManagementApplication.class);
-       // AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(fxmlexample.MaterialManagementApplication.class);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/dashboard.fxml"));
         fxmlLoader.setControllerFactory(springContext::getBean);
         root = fxmlLoader.load();
-        //appContext.getBean(MaterialController.class);
-        // root.setCenter(new MaterialView().asParent());
-        //root = new MaterialView().asParent();
-
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Hello World");
 		Scene scene = new Scene(root,800,600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -48,7 +37,4 @@ public class MaterialManagementApplication extends Application {
 	public void stop() throws Exception {
 		springContext.stop();
 	}
-
-
-
 }
